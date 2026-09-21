f8e88b7198de538c5352b3870689554c794751a3
Andres Clavijo <ylaeccompany@gmail.com>
2026-09-20 23:33:02 -0500

    nueva tarea integrada

---
+++ calculando.py
@@ -0,0 +1,10 @@
+def calcular_promedio(nota1, nota2, nota3):
+    return (nota1 + nota2 + nota3) / 3
+
+nota1 = 8
+nota2 = 9
+nota3 = 10
+
+promedio = calcular_promedio(nota1, nota2, nota3)
+
+print("El promedio de las tres notas es:", promedio)
